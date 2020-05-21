fe0282f67c169c2b8a2babc869eb259989fc0c55
Hiroki Nakashima <hiroki123naka4ma@gmail.com>
2020-05-21 12:15:16 +0900

    basics

--- src/index.ts
+++ src/index.ts
@@ -1,94 +1,68 @@
-import { WebGLRenderer, Scene, PerspectiveCamera, Texture, MeshBasicMaterial, Mesh, BoxBufferGeometry } from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import gsap from 'gsap';
+import { 
+  WebGLRenderer, 
+  PerspectiveCamera, 
+  Scene, 
+  DirectionalLight, 
+  BoxGeometry, 
+  MeshLambertMaterial,
+  Mesh,
+} from 'three';
 
-// https://threejs.org/examples/#webgl_panorama_cube
-// https://github.com/mrdoob/three.js/blob/master/examples/webgl_panorama_cube.html
-
-let camera: PerspectiveCamera;
+// variables
 let renderer: WebGLRenderer;
+let camera: PerspectiveCamera;
 let scene: Scene;
-let controls: OrbitControls;
+let cube: Mesh;
 
 init();
 animate();
 
 function init() {
-  const container = document.getElementById('container');
-
-  renderer = new WebGLRenderer();
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  container?.appendChild(renderer.domElement);
-
+  // シーンの作成
   scene = new Scene();
 
-  camera = new PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 100);
-  camera.position.z = 0.01;
+  // カメラの作成
+  camera = new PerspectiveCamera(50, window.innerWidth/window.innerHeight, 0.1, 1000);
+  camera.position.set(0, 0, 400);
+  camera.lookAt(0, 0, 0);
 
-  controls = new OrbitControls(camera, renderer.domElement);
-  controls.enableZoom = false;
-  controls.enablePan = false;
-  controls.enableDamping = true;
-  controls.rotateSpeed = -0.25;
+  // レンダラーの作成
+  renderer = new WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  const container = document.getElementById("container");
+  // #container の中に配置
+  container?.appendChild(renderer.domElement);
 
-  var textures = getTexturesFromAtlasFile( "images/sun_temple_stripe.jpg", 6 );
 
-  var materials = [];
+  // ライトの作成
+  const light = new DirectionalLight(0xffffff, 0.8);
+  light.position.set(0, 100, 500);
+  // シーンにライトを追加
+  scene.add( light );
 
-  for (let i = 0; i < 6; i++) {
-    materials.push(new MeshBasicMaterial({ map: textures[i] }));
-  }
+  // キューブの作成
+  const geometry = new BoxGeometry(100, 100, 100);
+  const material = new MeshLambertMaterial({ color: 0xffee00 });
+  cube = new Mesh(geometry, material);
+  // シーンにキューブを追加
+  scene.add( cube );
 
-  var skyBox = new Mesh(new BoxBufferGeometry(1, 1, 1), materials);
-  skyBox.geometry.scale(1, 1, -1);
-  scene.add(skyBox);
-
+  // window resize
   window.addEventListener('resize', onWindowResize, false);
 }
-
-function getTexturesFromAtlasFile(atlasImgUrl: string, tilesNum: number) {
-  var textures: any[] = [];
-
-  for (let i = 0; i < tilesNum; i++) {
-    textures[i] = new Texture();
-  }
-
-  var imageObj = new Image();
-  imageObj.onload = function() {
-    var canvas;
-    var context;
-    var titleWidth = imageObj.height;
-
-    for (let i = 0; i < textures.length; i++) {
-      canvas = document.createElement('canvas');
-      context = canvas.getContext("2d");
-      canvas.width = titleWidth;
-      canvas.height = titleWidth;
-      context?.drawImage(imageObj, titleWidth * i, 0, titleWidth, titleWidth, 0, 0, titleWidth, titleWidth);
-      textures[i].image = canvas;
-      textures[i].needsUpdate = true;
-    }
-  }
-
-  imageObj.src = atlasImgUrl;
-
-  return textures;
-}
-
 
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
-
 
 function animate() {
   requestAnimationFrame(animate);
 
-  controls.update();
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
 
+  // レンダリング実行
   renderer.render(scene, camera);
 }
